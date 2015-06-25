337b70aef87932f2d42d1683427038d44eb6f407
Raghu Vamshi <iamraghu@live.com>
2015-06-25 10:49:43 +0545

    Create PRIME1.cpp

---
+++ PRIME1.cpp
@@ -0,0 +1,26 @@
+//http://www.codechef.com/problems/PRIME1
+// again trying to min. runtime
+#include<stdio.h>
+main()
+{
+	long long int t,a,b,n,i,j,just;
+	scanf("%lld",&t);
+	while(t--){
+		scanf("%lld %lld",&a,&b);
+		if(a==1)a=2;
+		long long int prime[b-a+1];
+		for(i=0;i<b-a+1;i++) prime[i]=0;
+		for(i=2;i*i<=b;i++)
+		{
+			just=a/i;	just*=i;
+			for(j=just;j<=b;j+=i)
+			  if(j!=i)
+				 prime[j-a]=1;
+		}
+		
+		for(i=0;i<b-a+1;i++)
+		  if(prime[i]==0)
+			printf("%lld\n",i+a);
+		
+	}
+}
